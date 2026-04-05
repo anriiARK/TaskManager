b0a67c8258f7c2d55103bc81de891fd207205845
Anrii <73745232+ARQOdev@users.noreply.github.com>
2026-04-05 15:20:20 +0400

    Setup

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Task Tormeti");
+
+        frame.setSize(600, 400);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+}
